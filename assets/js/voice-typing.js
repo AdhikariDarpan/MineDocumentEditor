8d3f6830e354148526d91fd7f235a3ecc610fc2c
AdhikariDarpan <124805232+AdhikariDarpan@users.noreply.github.com>
2024-12-01 20:26:24 +0545

    Update voice-typing.js

--- assets/js/voice-typing.js
+++ assets/js/voice-typing.js
@@ -1,4 +1,4 @@
-import { speechToText } from "https://unpkg.com/speech-into-text@latest/index.js";
+import { speechToText } from "https://unpkg.com/speech-into-text@1.4.4/index.js";
 
 const voiceTyping = document.getElementById("voiceTyping");
 let Clicked = true;
