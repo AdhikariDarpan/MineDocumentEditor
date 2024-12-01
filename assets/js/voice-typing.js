5488131369b6f4e6a95c0e76a3a254d44ef3a0e5
AdhikariDarpan <124805232+AdhikariDarpan@users.noreply.github.com>
2024-12-01 19:53:03 +0545

    Add files via upload

---
+++ assets/js/voice-typing.js
@@ -0,0 +1,25 @@
+import { speechToText } from "https://unpkg.com/speech-into-text@1.4.3/index.js";
+
+const voiceTyping = document.getElementById("voiceTyping");
+let Clicked = true;
+voiceTyping.addEventListener("click", (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+  let content = `
+  <div class="text-content">Voice Typing Language Selection</div>
+  <div style="display:flex; align-items:center;">
+    <label for="selectLang">Choose Your Language:</label>
+    <select id="selectLang"></select>
+  </div>
+  <div class="btns">
+    <button id="startVoiceTyping">Start/Stop</button>
+  </div>`;
+  showDialog(content);
+  speechToText('text-field', '', 'startVoiceTyping', '', 'selectLang'); 
+  typingFunction();
+});
+
+function typingFunction(){
+  document.getElementById('text-field').blur();
+}
+
